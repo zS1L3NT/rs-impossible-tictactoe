c51cc0ad08a35220477e254c92e87e1f199f69c8
Zechariah <zechariahtan144@gmail.com>
2022-05-15 23:37:20 +0800

    ✨ clear screen when starting script

--- src/main.rs
+++ src/main.rs
@@ -172,6 +172,7 @@ fn main() {
     let mut game = Game::new();
 
     loop {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
         println!("O always gets the first move, then X");
         print!("Who starts first, user or computer? (u/c): ");
         stdout().flush().expect("Failed to flush input");
